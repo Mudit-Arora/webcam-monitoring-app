0e3b997199b05219fa3ed1d0d68fdbd6cd4f0436
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2023-07-21 13:12:58 -0700

    Capture video through camera

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ import time
 
 # main camera
 video = cv2.VideoCapture(0)
-check, frame = video.read()
 time.sleep(1)
 
-print(check)
-print(frame)
+while True:
+    check, frame = video.read()
+    # processes video
+    cv2.imshow("My video", frame)
+
+    key = cv2.waitKey(1)
+    # camera turns off when q key is pressed
+    if key == ord("q"):
+        break
+
+# shows your video using camera
+video.release()
+
